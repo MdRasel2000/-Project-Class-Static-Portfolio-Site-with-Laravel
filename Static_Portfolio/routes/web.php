f32d8a2cce0f1e6490aefd3efb06ed9c6a251788
MdRasel2000 <ctgmdraselislam@gmail.com>
2025-04-11 23:56:09 +0600

    Static Portfolio Site Whit Laravel

---
+++ Static_Portfolio/routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\SiteController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [SiteController::class, 'homePage'])->name('home');
+Route::get('/contact', [SiteController::class, 'contactPage'])->name('contact');
+Route::get('/about', [SiteController::class, 'aboutPage'])->name('about');
+Route::get('/services', [SiteController::class, 'servicesPage'])->name('services');
+Route::get('/portfolio', [SiteController::class, 'portfolioPage'])->name('portfolio');
